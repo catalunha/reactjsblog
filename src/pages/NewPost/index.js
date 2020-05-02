98d8f1c3c4f5132a73aa84c969d30c6eb5ffb6a6
catalunha <catalunha.mj@gmail.com>
2020-05-02 09:41:52 -0300

    aula80

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -12,7 +12,8 @@ class NewPost extends Component {
       arquivo: null,
       arquivoPath: '',
       descricao: '',
-      alert: ''
+      alert: '',
+      progress: ''
     }
     this.cadastrar = this.cadastrar.bind(this)
     this.handleFile = this.handleFile.bind(this)
@@ -32,6 +33,7 @@ class NewPost extends Component {
       await posts.child(chave).set({
         titulo: this.state.titulo,
         imagem: this.state.imagem,
+        arquivoPath: this.state.arquivoPath,
         descricao: this.state.descricao,
         autor: localStorage.nome
       })
@@ -53,24 +55,30 @@ class NewPost extends Component {
     }
   }
   handleUpload = async () => {
-    const {arquivo}  = this.state
+    const { arquivo } = this.state
     const currentUid = firebase.getCurrentUid()
-
     const uploadTasks = firebase.storage.ref(`posts/${currentUid}/${arquivo.name}`).put(arquivo);
 
-    // await uploadTasks.on('state_changed',
-    //   (snapshot) => {
-    //     //Progress
-    //     console.log('Upload em progresso')
+    await uploadTasks.on('state_changed',
+      (snapshot) => {
+        //Progress
+        console.log('Upload em progresso')
+        const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+        this.setState({progress:progress})
+      }, (error) => {
+        //Erro
+        console.log('Error imagem: ' + error)
 
-    //   }, (error) => {
-    //     //Erro
-    //     console.log('Error imagem: ' + error)
-
-    //   }, (sucess) => {
-    //     //Sucesso
-    //     console.log('Sucesso no upload')
-    //   })
+      }, (sucess) => {
+        //Sucesso
+        console.log('Sucesso no upload')
+        firebase.storage
+          .ref(`posts/${currentUid}`)
+          .child(`${arquivo.name}`)
+          .getDownloadURL().then((url) => {
+            this.setState({ arquivoPath: url })
+          })
+      })
 
   }
   render() {
@@ -87,6 +95,12 @@ class NewPost extends Component {
           <input type='text' placeholder='Url da capa' value={this.state.imagem} autoFocus onChange={(e) => this.setState({ imagem: e.target.value })} /><br />
           <label>Arquivo:</label><br />
           <input type='file' onChange={this.handleFile} /><br />
+          {/* <label>Preview:</label><br /> */}
+          {this.state.arquivoPath !== ''
+            ? <img src={this.state.arquivoPath} alt='' width='250' height='150' />
+            : <progress value={this.state.progress} max='100' />
+          }
+
           <label>Descrição:</label><br />
           <textarea type='text' placeholder='Alguma descrição' value={this.state.descricao} autoFocus onChange={(e) => this.setState({ descricao: e.target.value })} /><br />
           <button type='submit'>Cadastrar</button>
